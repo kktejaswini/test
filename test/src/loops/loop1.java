0b7b7389c1b11da22bcbaff602696b1adfe7c2d4
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-02 22:29:54 +0530

    first commit

---
+++ test/src/loops/loop1.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class loop1 {
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		if(n<0) {
+			System.out.println("Negative");
+		}
+		else if(n==0) {
+			System.out.println("zero");
+		}
+		else {
+			System.out.println("positive");
+		}
+
+	}
+}
