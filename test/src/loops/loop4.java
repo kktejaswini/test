6e356e8836599c14628c80c9b22720eacf4edf18
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-02 22:33:28 +0530

    first commit

---
+++ test/src/loops/loop4.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class loop4 {
+	public static void main(String[] args) {
+		char a=args[0].charAt(0);
+		char b=args[1].charAt(0);
+		if (a < b) {
+			System.out.println(a+","+b);
+			
+		}
+		else {
+			System.out.println(b+","+a);
+		}
+
+	}
+}
