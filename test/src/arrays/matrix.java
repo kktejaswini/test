b5c44ea0db7a7d15f84dd1b462094e21f19f887d
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-02 20:57:23 +0530

    first commit

---
+++ test/src/arrays/matrix.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class matrix {
+	public static void main(String[] args) {
+		int arr[][] = new int[2][2];
+		int n=args.length;
+		if (n<4) {
+			System.out.println("enter 4 numbers");
+		}
+		int k=0;
+		for (int i=0;i<2;i++) {
+			for (int j=0;j<2;j++) {
+				arr[i][j]=Integer.parseInt(args[k++]);
+
+			}
+
+	}
+
+	}
+}
