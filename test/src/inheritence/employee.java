7cf6220e359431a98c37af64bd8307d421111064
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-02 22:25:54 +0530

    first commit

--- test/src/inheritence/employee.java
+++ test/src/inheritence/employee.java
@@ -1,5 +1,21 @@
 package inheritence;
 
 public class employee {
-
+	private double annualsalary;
+	private int yearofjoining;
+	private String nationalInsuranceNo;
+	
+	 employee(String name,double annualsalary,int yearofjoining,String nationalInsuranceNo){
+		
+		super(name);
+		this.annualsalary=annualsalary;
+		this.yearofjoining=yearofjoining;
+		this.nationalInsuranceNo=nationalInsuranceNo;
+	}
+	
+	void display() {
+		
+		System.out.println("Name : "+name+"\n"+"Annual Salary : "+annualsalary+"\n"+"Year Of joining : "+yearofjoining+"\n"+"National Id : "+nationalInsuranceNo);
+		
+	}
 }
---
+++ test/src/inheritence/testemployee.java
@@ -0,0 +1,10 @@
+package inheritence;
+
+import com.wipro.inheritence.employee;
+
+public class testemployee {
+	public static void main(String[] args) {
+		employee obj = new employee("sai",100000,2015,"asdfg");
+		obj.display();
+}
+}
